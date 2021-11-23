fad18b7c346c8b8ca5a64a1bd6bb0000f59e4c4e
semvlu <74308718+semvlu@users.noreply.github.com>
2021-11-23 16:20:56 +0800

    Add files via upload

---
+++ B10933001_祁柏君/04.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+	int a, b,c,d;
+	scanf("%d %d %d %d", &a,&b,&c,&d);
+	int e[a][b];
+	int f[c][d];
+	int g[a][d];
+	int i,j,k,l,m,n,o,p,q,r;
+	
+	for(i = 0; i < a; i++)
+	{
+		for(j = 0;j < b;j++)
+		{
+			scanf("%d", &e[i][j]);	
+		}
+	}
+	for(k = 0; k < c; k++)
+	{
+		for(l = 0;l < d;l++)
+		{
+			scanf("%d", &f[k][l]);	
+		}
+	}
+	
+	for(m = 0; m < a;m++)
+	{
+		for(n = 0;n < d;n++)
+		{
+			for(o = 0; o < b;o++)
+			{
+				g[m][n] += e[m][o] * f[o][n];
+			}	
+		}
+	}
+	
+	for(q = 0; q < a; q++)
+	{
+		for(r = 0; r < d; r++)
+		{
+			printf("%d ", g[q][r]);
+		}
+		printf("\n");
+	}
+}
