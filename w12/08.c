7f8cebc326b536b41bc3e556c5fd7cdfb7966e4e
semvlu <74308718+semvlu@users.noreply.github.com>
2021-12-21 12:02:50 +0800

    Update 08.c

--- w12/08.c
+++ w12/08.c
@@ -1,31 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-// order: article, noun, verb, preposition, article, noun
-// i tried 2 ways, one way: line 23~24, another: line 26 -> runtime error on OJ
+#include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-    char *art[5] = {"the", "a", "one", "some", "any"};
-    char *n[5] = {"boy", "girl", "dog", "town", "car"};
-    char *v[5] = {"drove", "jumped", "ran", "walked", "skipped"};
-    char *prep[5] = {"to", "from", "over", "under", "on"};
-    int rart, rn, rv, rprep, rart1, rn1;
-    char cap;
-    for (int i = 0; i < 20; i++)
+	char *art[5]={"the","a","one","some","any"};
+	char *n[5]={"boy","girl","dog","town","car"};
+	char *v[5]={"drove","jumped","ran","walked","skipped"};
+	char *prep[5]={"to","from","over","under","on"};
+	for(int i = 0;i < 20; i++)
     {
-        rart = (rand() % 5);
-        rn = (rand() % 5);
-        rv = (rand() % 5);
-        rprep = (rand() % 5);
-        rart1 = (rand() % 5);
-        rn1 = (rand() % 5);
-        //cap = toupper(art[rart][0]);
-        //memset(art[rart], cap, sizeof(art)[rart][0]);
-
-        //art[rart][0] = art[rart][0] - 32;
-
-        printf("%s %s %s %s %s %s.\n", 
-        art[rart], n[rn], v[rv], prep[rprep], art[rart1], n[rn1]);
-    }
+		int ra = rand() % 5;
+		int ra1 = rand() % 5;
+		int rn = rand() % 5;
+		int rn1 = rand() % 5;
+		int rv = rand() % 5;
+		int rp = rand () % 5;
+		if(art[ra] == "the")
+        {
+			printf("%s %s %s %s %s %s.\n","The",n[rn],v[rv],prep[rp],art[ra1],n[rn1]);
+		}
+		else if(art[ra] == "any")
+        {
+			printf("%s %s %s %s %s %s.\n","Any",n[rn],v[rv],prep[rp],art[ra1],n[rn1]);
+		}
+		else if(art[ra] == "a")
+        {
+			printf("%s %s %s %s %s %s.\n","A",n[rn],v[rv],prep[rp],art[ra1],n[rn1]);
+		}
+		else if(art[ra] == "one")
+        {
+			printf("%s %s %s %s %s %s.\n","One",n[rn],v[rv],prep[rp],art[ra1],n[rn1]);
+		}
+		else
+        {
+			printf("%s %s %s %s %s %s.\n","Some",n[rn],v[rv],prep[rp],art[ra1],n[rn1]);
+		}
+	}
 }
