b9a13cc5fdcd31fc620df5c5972d9eb7b78ea371
semvlu <74308718+semvlu@users.noreply.github.com>
2022-03-10 18:12:31 +0800

    Add files via upload

--- cpp/W03Lab05.cpp
+++ cpp/W03Lab05.cpp
@@ -6,10 +6,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sstream>
+#include <iomanip>
 #include <cassert>
+#include <cmath>
 using namespace std;
-char type[5];
+char type;
 typedef struct Square
 {
 	int w, x, y;
@@ -36,9 +37,7 @@ void draw(vector<vector<char> > map)
 	for (int i = 0; i < map.size(); i++)
 	{
 		for (int j = 0; j < map[i].size(); j++)
-		{
 			cout << map[i][j];
-		}
 		cout << endl;
 	}
 }
@@ -46,24 +45,24 @@ void draw(vector<vector<char> > map)
 int main()
 {
 	int m, n;
-
 	cin >> m >> n;
 	vector <vector <char> > map(m, vector<char>(n, '*'));
 
 	while (cin >> type)
 	{
-		assert(type != "EXIT");
-
 		T t;
-		switch (str2int(type))
+		switch (type)
 		{
-		case str2int("S"):
+		case ('S'):
 			S s;
 			cin >> s.w >> s.x >> s.y;
-			if (s.x >= n || s.y >= m || s.x + s.w - 1 >= n || s.y + s.w - 1 >= m)
+			if (s.w == 1)
 			{
-				cout << "Out of range" << endl;
+				map[s.y][s.x] = 'X';
+				draw(map);
 			}
+			else if (s.x >= n || s.y >= m || s.x + s.w - 1 >= n || s.y + s.w - 1 >= m || s.w > m || s.w > n)
+				cout << "Out of range." << endl;
 			else
 			{
 				for (int i = s.y; i < s.y + s.w; i++)
@@ -79,116 +78,122 @@ int main()
 				draw(map);
 			}
 			break;
-		case str2int("T"):
+		case ('T'):
 			cin >> t.w >> t.x >> t.y;
 			cin >> t.face;
 
-			switch (str2int(t.face))
+			if (t.w == 1)
 			{
-			case str2int("LU"):
-				if (t.x - t.w + 1 < 0 || t.y - t.w + 1 < 0)
-					cout << "Out of range" << endl;
-				else
-				{
-					for (int i = t.y; i > t.y - t.w; i--)
-					{
-						map[i][t.x] = 'X';
-						for (int j = t.x - t.w + 1; j <= t.x; j++)
-						{
-							if (j + i == t.x + t.y - t.w + 1)
-							{
-								map[i][j] = 'X';
-							}
-						}
-					}
-					for (int i = t.x; i > t.x - t.w; i--)
-					{
-						map[t.y][i] = 'X';
-					}
-				}
-				break;
-			case str2int("LD"):
-				if (t.x - t.w + 1 < 0 || t.y + t.w - 1 > m - 1)
-					cout << "Out of range" << endl;
-				else
-				{
-					for (int i = t.y; i <  t.y + t.w; i++)
-					{
-						map[i][t.x] = 'X';
-						for (int j = t.x - t.w + 1; j <= t.x; j++)
-						{
-							if (j + t.w == i)
-							{
-								map[i][j] = 'X';
-							}
-						}
-					}
-					for (int i = t.x; i > t.x - t.w; i--)
-					{
-						map[t.y][i] = 'X';
-					}
-				}
-				break;
-			case str2int("RU"):
-				if (t.x + t.w - 1 > n - 1 || t.y - t.w + 1 < 0)
-				{
-					cout << "Out of range" << endl;
-				}
-				else
-				{
-					for (int i = t.y; i > t.y - t.w; i--)
-					{
-						map[i][t.x] = 'X';
-						for (int j = t.x; j <= t.x + t.w; j++)
-						{
-							if (j == i + t.w - 1)
-							{
-								map[i][j] = 'X';
-							}
-						}
-					}
-					for (int i = t.x; i < t.x + t.w; i++)
-					{
-						map[t.y][i] = 'X';
-					}
-				}
-				break;
-			case str2int("RD"):
-				if (t.x + t.w - 1 > n - 1 || t.y + t.w - 1 > m - 1)
-					cout << "Out of range" << endl;
-				else
-				{
-					for (int i = t.y; i < t.y + t.w; i++)
-					{
-						map[i][t.x] = 'X';
-						for (int j = t.x; j <= t.x + t.w; j++)
-						{
-							if (j + i == t.x + t.y + t.w - 1)
-							{
-								map[i][j] = 'X';
-							}
-						}
-					}
-					for (int i = t.x; i < t.x + t.w; i++)
-					{
-						map[t.y][i] = 'X';
-					}
-				}
-				break;
-			default:
-				break;
+				map[t.y][t.x] = 'X';
+				draw(map);
 			}
-			draw(map);
+			else if (t.w > m || t.w > n || t.x > n - 1 || t.y > m - 1)
+				cout << "Out of range." << endl;
+			else
+			{
+				switch (str2int(t.face))
+				{
+				case str2int("LU"):
+					if (t.x - t.w + 1 < 0 || t.y - t.w + 1 < 0)
+						cout << "Out of range." << endl;
+					else
+					{
+						for (int i = t.y; i > t.y - t.w; i--)
+						{
+							map[i][t.x] = 'X';
+							for (int j = t.x - t.w + 1; j <= t.x; j++)
+							{
+								if (j + i == t.x + t.y - t.w + 1)
+								{
+									map[i][j] = 'X';
+								}
+							}
+						}
+						for (int i = t.x; i > t.x - t.w; i--)
+						{
+							map[t.y][i] = 'X';
+						}
+					}
+					break;
+				case str2int("LD"):
+					if (t.x - t.w + 1 < 0 || t.y + t.w + 1 > m)
+						cout << "Out of range." << endl;
+					else
+					{
+						for (int i = t.y; i < t.y + t.w; i++)
+						{
+							map[i][t.x] = 'X';
+							for (int j = t.x - t.w + 1; j <= t.x; j++)
+							{
+								if (i - j == t.y - t.x + t.w - 1)
+								{
+									map[i][j] = 'X';
+								}
+							}
+						}
+						for (int i = t.x; i > t.x - t.w; i--)
+						{
+							map[t.y][i] = 'X';
+						}
+					}
+					break;
+				case str2int("RU"):
+					if (t.x + t.w + 1 > n || t.y - t.w + 1 < 0)
+						cout << "Out of range." << endl;
+					else
+					{
+						for (int i = t.y; i > t.y - t.w; i--)
+						{
+							map[i][t.x] = 'X';
+							for (int j = t.x + t.w - 1; j >= t.x; j--)
+							{
+								if (i - j == t.y - t.x - t.w + 1)
+								{
+									map[i][j] = 'X';
+								}
+							}
+						}
+						for (int i = t.x; i < t.x + t.w; i++)
+						{
+							map[t.y][i] = 'X';
+						}
+					}
+					break;
+				case str2int("RD"):
+					if (t.x + t.w + 1 > n || t.y + t.w + 1 > m)
+						cout << "Out of range." << endl;
+					else
+					{
+						for (int i = t.y; i < t.y + t.w; i++)
+						{
+							map[i][t.x] = 'X';
+							for (int j = t.x; j <= t.x + t.w; j++)
+							{
+								if (j + i == t.x + t.y + t.w - 1)
+								{
+									map[i][j] = 'X';
+								}
+							}
+						}
+						for (int i = t.x; i < t.x + t.w; i++)
+						{
+							map[t.y][i] = 'X';
+						}
+					}
+					break;
+				default:
+					break;
+				}
+				draw(map);
+			}
 			break;
-		case str2int("L"):
+		case ('L'):
 			L l;
 			cin >> l.x1 >> l.y1 >> l.x2 >> l.y2;
 
 			if (l.x1 >= n || l.x2 >= n || l.y1 >= m || l.y2 >= m ||
 				abs(l.x1 - l.x2) >= n || abs(l.y1 - l.y2) >= m)
-			{
-				cout << "Out of range" << endl;
-			}
+				cout << "Out of range." << endl;
 			else
 			{
 				if (l.x2 > l.x1)
@@ -217,7 +222,7 @@ int main()
 					}
 					else // y2 = y1
 					{
-						for (int i = l.x1; i <= l.x2 + 1; i++)
+						for (int i = l.x1; i <= l.x2; i++)
 						{
 							map[l.y1][i] = 'X';
 						}
